5dfc58af9297a5032bf86ecbb1ff3268d1b805c3
Tomohiro Degawa <degawa.tomohiro@gmail.com>
2023-03-13 19:58:25 +0900

    refactored random-like positive integer generation

--- src/newunit.f90
+++ src/newunit.f90
@@ -13,16 +13,12 @@ contains
 
         integer(int32) :: stat
         logical :: opened
-        real(real64) :: rand
 
         opened = .true.; stat = -1
         do while (opened .or. stat /= 0)
-            call random_number(rand)
+            unit_number = random_like_positive_number_int32()
 
-            ! converting upper 32 bits of 64-bit floating-point number to an integer
-            unit_number = abs(transfer(rand, mold=unit_number))
-
-            if (any([unit_number <= 0, & ! consider unit_number = -2147483648
+            if (any([unit_number <= 0, &
                      unit_number == input_unit, &
                      unit_number == output_unit, &
                      unit_number == error_unit])) &
@@ -31,5 +27,28 @@ contains
             inquire (unit=unit_number, opened=opened, iostat=stat)
             ! retry if unit_number have already been opened or inquire failed
         end do
+    contains
+        function random_like_positive_number_int32() result(num)
+            integer(int32) :: num
+                !! pseudorandom-like number
+
+            block
+                real(real64) :: rand
+                call random_number(rand)
+
+                ! converting upper 32 bits of 64-bit floating-point number to an integer
+                num = transfer(rand, mold=unit_number)
+            end block
+
+            block
+                integer(int32) :: sign_bit
+                sign_bit = bit_size(num) - 1 ! 0-origin
+
+                ! set the sign bit to zero for converting to a positive value.
+                ! the abs() is not used because the absolute value cannot obtain
+                ! when unit_number = -2147483648.
+                num = ibclr(i=num, pos=sign_bit)
+            end block
+        end function random_like_positive_number_int32
     end function get_newunit_number
 end module newunit
